a68d4b0a6f7056fc68b6de686571f5bef96cb2ba
pch <fehyun3@gmail.com>
2024-06-19 21:43:34 +0900

    feat: Add configuration for ESlint, Jest, TypeScript, and other dependencies

---
+++ .eslintrc.cjs
@@ -0,0 +1,55 @@
+const path = require("path");
+
+module.exports = {
+  overrides: [
+    // config files
+    {
+      files: ["./{.*,*}.{js,cjs}"],
+      parserOptions: {
+        ecmaVersion: "latest",
+      },
+      extends: ["airbnb-base", "plugin:prettier/recommended"],
+      env: {
+        node: true,
+      },
+      rules: {
+        "prettier/prettier": "error",
+      },
+    },
+    // ts
+    {
+      files: ["./src/**/*.ts", "./src/**/*.d.ts"],
+      parser: "@typescript-eslint/parser",
+      plugins: ["@typescript-eslint"],
+      extends: [
+        "airbnb-base",
+        "airbnb-typescript/base",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:prettier/recommended",
+      ],
+      parserOptions: {
+        project: [path.resolve(__dirname, "./tsconfig.json")],
+      },
+      rules: {
+        "prettier/prettier": "error",
+      },
+      env: {
+        node: true,
+      },
+    },
+    // js
+    {
+      files: ["./src/**/*.js"],
+      extends: ["airbnb-base", "plugin:prettier/recommended"],
+      parserOptions: {
+        ecmaVersion: "latest",
+      },
+      rules: {
+        "prettier/prettier": "error",
+      },
+      env: {
+        node: true,
+      },
+    },
+  ],
+};
