b82f8c0e8d77256f7e9998d69b50acdf66f1d4c9
chul-hyun-93 <chulhyun.park93@gmail.com>
2024-06-24 22:37:02 +0900

    feat: Add solution for Leetcode problem 1768

---
+++ src/leetcode/1768/code.test.ts
@@ -0,0 +1,45 @@
+function mergeAlternately(word1: string, word2: string): string {
+  const list1 = word1.split('');
+  const list2 = word2.split('');
+
+  const len = list1.length + list2.length;
+  let result: string[] = [];
+  for(let i = 0; i < len; i++) {
+    const index = Math.floor(i / 2);
+    if(i % 2 === 0) {
+      const char = list1.shift();
+      if(char === undefined) {
+        result = result.concat(list2);
+        break;
+      }else {
+        result.push(char);
+      }
+    } else {
+      const char = list2.shift();
+      if(char === undefined) {
+        result = result.concat(list1);
+        break;
+      }else {
+        result.push(char);
+      }
+    }
+  }
+
+  return result.join('');
+};
+
+test('case 1', () => {
+  expect(mergeAlternately("abc", "pqr")).toStrictEqual("apbqcr");
+});
+
+test('case 2', () => {
+  expect(mergeAlternately("ab","pqrs" )).toStrictEqual("apbqrs");
+});
+
+test('case 3', () => {
+  expect(mergeAlternately("abcd", "pq")).toStrictEqual("apbqcd");
+});
+
+test('case 4', () => {
+  expect(mergeAlternately("cf", "eee")).toStrictEqual("cefee");
+});
---
+++ src/leetcode/1768/problem.md
@@ -0,0 +1,51 @@
+link: https://leetcode.com/problems/merge-strings-alternately/
+
+![img.png](img.png)
+
+두 문자열 word1과 word2가 주어집니다. 두 문자열을 번갈아 가며 문자를 추가하여 병합합니다. word1부터 시작합니다. 만약 한 문자열이 다른 문자열보다 길다면, 추가된 문자를 병합된 문자열 끝에 추가합니다.
+
+병합된 문자열을 반환하세요.
+
+### 예시 1:
+
+입력: word1 = "abc", word2 = "pqr"
+
+출력: "apbqcr"
+
+설명: 병합된 문자열은 다음과 같이 병합됩니다:
+```
+word1:  a   b   c
+word2:    p   q   r
+merged: a p b q c r
+```
+
+### 예시 2:
+
+입력: word1 = "ab", word2 = "pqrs"
+
+출력: "apbqrs"
+
+설명: word2가 더 길기 때문에 "rs"가 끝에 추가됩니다.
+```
+word1:  a   b 
+word2:    p   q   r   s
+merged: a p b q   r   s
+```
+
+### 예시 3:
+
+입력: word1 = "abcd", word2 = "pq"
+
+출력: "apbqcd"
+
+설명: word1이 더 길기 때문에 "cd"가 끝에 추가됩니다.
+```
+word1:  a   b   c   d
+word2:    p   q 
+merged: a p b q c   d
+```
+
+### 제약 조건:
+
+- 1 <= word1.length, word2.length <= 100
+- word1과 word2는 소문자 영어 문자로만 구성됩니다.
