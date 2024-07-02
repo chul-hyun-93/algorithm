5db3fca01cc2b27e9d723110f436477fa3597791
chul-hyun-93 <chulhyun.park93@gmail.com>
2024-07-02 22:50:04 +0900

    Optimize commonChars function in leetcode 1002 test code

--- src/leetcode/1002/code.test.ts
+++ src/leetcode/1002/code.test.ts
@@ -1,19 +1,27 @@
 function commonChars(words: string[]): string[] {
-  let target = words[0].split("");
-  for (let i = 1; i < words.length; i++) {
-    target = commonChars2(target, words[i].split(""));
-  }
-  return target;
-}
+  const wordList = words.map((word) => word.split(""));
+  let common = wordList[0].reduce<Record<string, number>>((acc, char) => {
+    acc[char] = (acc[char] || 0) + 1;
+    return acc;
+  }, {});
 
-function commonChars2(word1: string[], word2: string[]): string[] {
-  return word1.filter((char) => {
-    const index = word2.indexOf(char);
-    if (index < 0) return false;
-    // eslint-disable-next-line no-param-reassign
-    word2 = [...word2.slice(0, index), ...word2.slice(index + 1)];
-    return true;
-  });
+  for (let i = 1; i < wordList.length; i++) {
+    const newCommon: Record<string, number> = {}; // 공통되지 않은 숫자에서 제거하는 방식으로 변경해보기 (숫자가 낮아지게)
+    const word = wordList[i];
+    for (const char of word) {
+      if (common[char] === undefined) continue;
+      if (newCommon?.[char] === undefined || newCommon[char] < common[char]) {
+        newCommon[char] = (newCommon[char] ?? 0) + 1;
+      }
+    }
+
+    common = newCommon;
+  }
+
+  return Object.entries(common).reduce<string[]>((acc, [char, count]) => {
+    acc.push(...Array(count).fill(char));
+    return acc;
+  }, []);
 }
 
 test("case 1", () => {
