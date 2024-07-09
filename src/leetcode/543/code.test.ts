41b4c17d03b4ed1095ffc3d13a32aabf95a8316a
chul-hyun-93 <chulhyun.park93@gmail.com>
2024-07-09 22:28:57 +0900

    feat: Implement diameterOfBinaryTree function in leetcode 543 test

--- src/leetcode/543/code.test.ts
+++ src/leetcode/543/code.test.ts
@@ -12,7 +12,27 @@ class TreeNode {
   }
 }
 
-function diameterOfBinaryTree(root: TreeNode | null): number {}
+function diameterOfBinaryTree(root: TreeNode | null): number {
+  const valueRef = { value: 0 };
+  longestPath(root, valueRef);
+  return valueRef.value;
+}
+
+function longestPath(
+  root: TreeNode | null,
+  valueRef: { value: number },
+): number {
+  if (root === null) {
+    return 0;
+  }
+
+  const rightDiameter = longestPath(root.right, valueRef);
+  const leftDiameter = longestPath(root.left, valueRef);
+
+  valueRef.value = Math.max(valueRef.value, rightDiameter + leftDiameter);
+
+  return Math.max(rightDiameter, leftDiameter) + 1;
+}
 
 test("case 1", () => {
   const root = new TreeNode(
